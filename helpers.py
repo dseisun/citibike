fc37851b282a1ad34a6911657110ef686853ec60
Daniel Seisun <daseisun@gmail.com>
2022-10-03 14:07:07 -0400

    Added paretto helper and updated workbook

--- helpers.py
+++ helpers.py
@@ -7,3 +7,14 @@ def get_sample_file(sample_file = 'extracted_data/202207-citbike-tripdata.csv', 
         return pd.read_csv(sample_file, header=0, nrows=n)
     else:
         return pd.read_csv(sample_file, header=0)
+
+def paretto(df, col):
+    df = df.sort_values(col, ascending=False)
+    df['cumsum'] = df[col].cumsum()
+    df['cum_perc'] = 100*df['cumsum']/df[col].sum()
+    df['tmp'] = 1/len(df)
+    df['pct_total'] = df['tmp'].cumsum()
+    return df.set_index('pct_total')['cum_perc']
+
+
+
